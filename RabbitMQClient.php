8e72f65d82abfc9bf807fee3dd74bed9b650d178
Satchel <sq42@njit.edu>
2018-10-04 16:53:56 -0400

    auth working, redirect working.

--- RabbitMQClient.php
+++ RabbitMQClient.php
@@ -4,6 +4,12 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+function processresponse($response)
+{
+	echo "processing request or whatever \n";
+	var_dump($request);
+	echo $response['message'] . PHP_EOL; 
+}
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
@@ -16,14 +22,22 @@ else
 
 $request = array();
 $request['type'] = "login";
+//$request['username'] = 'b';
+//$request['password'] = '444';
+//$request['email'] = 'satchel@gmail';
+
 $request['username'] = $_POST['username'];
 $request['password'] = $_POST['password'];
 $request['email'] = $_POST['email'];
+//echo "a\n";
+$response = "no";
+//$client->process_response($response);
 $response = $client->send_request($request);
+//echo "b\n";
 //$response = $client->publish($request);
 
-echo "client received response: ".PHP_EOL;
-print_r($response);
+echo "client received response: returnCode: " . $response['returnCode'] . " message: " . $response['message'] .PHP_EOL;
+//print_r($response);
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
