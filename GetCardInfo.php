f256328a108601f9ee48ac39d065ca78572f70b0
Satchel <sq42@njit.edu>
2018-10-31 17:59:57 -0400

    updated getcardinfo

--- GetCardInfo.php
+++ GetCardInfo.php
@@ -1,6 +1,6 @@
 <?php
 
-$database = mysqli_connect('127.0.0.1', 'user', 'password', 'Project', '3306');
+$database = mysqli_connect('10.0.0.8', 'user', 'password', 'Project', '3306');
 if ($database->errno != 0)
 {
 	exit(0);
@@ -12,20 +12,23 @@ $response = $database->query($query);
 $IDCost = array();
 $ValidID = "";
 
-while ($row = mysqli_fetch_array(response, MYSQLI_ASSOC))
+while ($row = mysqli_fetch_array($response, MYSQLI_ASSOC))
 {
 	$IDCost[$row['ID']] = $row['price']; 
 }
+//print_r($IDCost);
 
 //append the where clause 
 foreach ($IDCost as $key => $value)
 {
 	$ValidID .= " ID = '$key' or";
 }
+//echo $ValidID;
 //take out the last ' or'
 $ValidID = substr($ValidID, 0, -3);
 
 $query = "select * from Cards where". $ValidID . ";";
+//echo $query;
 $response = $database->query($query);
 while ($row = mysqli_fetch_array($response, MYSQLI_ASSOC))
 {
@@ -40,7 +43,7 @@ while ($row = mysqli_fetch_array($response, MYSQLI_ASSOC))
 	$desc = $row['Description'];
 	$img = $row['ImageFilepath'];
 	
-	echo $id . "|" . $name . "|" . $type . "|" . $att . "|" . $def . "|" . $val . "|" . $fuse . "|" . $hp . "|" . $desc . "|" . $img . "|". $IDcost[$id] .";";
+	echo $id . "|" . $name . "|" . $type . "|" . $att . "|" . $def . "|" . $val . "|" . $fuse . "|" . $hp . "|" . $desc . "|" . $img . "|". $IDCost[$id] .";";
 }
 
 ?>
