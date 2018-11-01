6b507abeb27c6bbef946093c77c93a26ab805e59
mwk9 <mwk9@njit.edu>
2018-11-01 17:16:20 -0400

    Wrote a function that will grab data from the UserDeck table

--- MakeTransaction.php
+++ MakeTransaction.php
@@ -3,14 +3,23 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+function processresponse($response)
+{
+	echo "processing request or whatever \n";
+	var_dump($request);
+	echo $response['message'] . PHP_EOL; 
+}
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
 $request = array();
 $request['type'] = "transaction";
 $request['username'] = $_REQUEST['username']; //get from url string from unity cs
 $request['price'] = $_REQUEST['price'];
-$request['id'] = $_REQUEST['id'];
+$request['ID'] = $_REQUEST['id'];
+//$request['username'] = "b"; //get from url string from unity cs
+//$request['price'] = 50;
+//$request['ID'] = 1;
 
 $response = $client->send_request($request);
-echo "Client received response: returnCode: " $response['returnCode'] . " message: " . $response['message'] . PHP_EOL;
+echo "Client received response: returnCode: ". $response['returnCode'] . " message: " . $response['message'] . PHP_EOL;
 ?>
