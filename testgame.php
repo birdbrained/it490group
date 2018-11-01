945413bcaf052fc35a530710fe694029aeb6c545
NinJaguar360 <jaguars310@msn.com>
2018-11-01 15:59:06 -0400

    Adding css file and a test for the game php page

---
+++ testgame.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>IT490 Cooking Card Game</title>
+		<center><h2>The Game.</h2></center>
+		<link rel="stylesheet" href="game.css">
+	</head>
+	<body>
+		<?php
+		session_start();
+		echo "Session Set<br><br>";
+		echo '<br> <iframe src="Testbuild/index.html" height="100%" width="100%" ></iframe><br><br>';
+		?>
+	</body>
+</html>
