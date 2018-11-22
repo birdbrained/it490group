0e0b476b78e431eef74cdf009fa0c1f9c2929be6
mwk9 <mwk9@njit.edu>
2018-11-22 12:17:18 -0500

    Changed backup bash script to support getting files to compress from a text file

--- backup.sh
+++ backup.sh
@@ -4,13 +4,13 @@ cd ..
 OUTTAR="backup"
 INDIR="it490group/"
 
-if [ "$1" = "php" ]; then
-	OUTTAR="${OUTTAR}php"
-	INDIR="${INDIR}*.php"
+if [ "$1" = "server" ]; then
+	OUTTAR="${OUTTAR}_server"
+	INDIR="${INDIR}serverFiles.txt"
 	# tar -czf backupphp.tgz it490group/*.php
 fi
 OUTTAR="${OUTTAR}.tgz"
 # echo $OUTTAR
 # echo $INDIR
 
-tar -czf ${OUTTAR} $INDIR
+tar -zcvf ${OUTTAR} --files-from $INDIR
