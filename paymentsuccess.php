3b51637dd3ab06c2a7ef066cb3272669ef228b15
mwk9 <mwk9@njit.edu>
2018-12-13 20:57:41 -0500

    Updated paypal transaction to database

--- paymentsuccess.php
+++ paymentsuccess.php
@@ -8,7 +8,22 @@
 	</head>
 	<body>
 		<?php	
+			session_start();
+			require_once('path.inc');
+			require_once('get_host_info.inc');
+			require_once('rabbitMQLib.inc');
+
 			echo "Payment successful!<br>";
+
+			$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+			$request = array();
+			$request['type'] = "addfunds";
+			$request['amount'] = $_SESSION['amount'];
+			$request['email'] = $_SESSION['email'];
+			$request['message'] = "Adding funds...";
+			$response = $client->send_request($request);
+			echo "Client received response: returnCode: ". $response['returnCode'] . " message: " . $response['message'] . PHP_EOL;*/
+
 			echo "Returning to the game page...";
 			header( "refresh:5; url=game.php");
 		?>
